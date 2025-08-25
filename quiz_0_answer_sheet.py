40dad7c60b7e08d7b1753f88b07f517b2bbb94a4
Akeem Semper <akeem.semper@gmail.com>
2025-08-25 10:19:18 -0600

    update

--- quiz_0_answer_sheet.py
+++ quiz_0_answer_sheet.py
@@ -1,6 +1,19 @@
 import pandas as pd
 import numpy as np
 
+'''
+This is a sample answer sheet for the autotested portion of the quiz.
+
+For any parts where you are asked to write a function, please write your function
+in the space provided below. Do not change the names or signatures of the functions if
+they are specified in the question.
+
+Your notebook file should use these functions, but they need to be independent of the notebook
+and callable from anywhere. The assumption is that these files are general tools that could be used
+in any project, and you are importing them into your notebook to use them in whatever analysis you are doing.
+'''
+
+
 def numberAdder(num_1, num_2):
     """
     Adds two numbers together.
